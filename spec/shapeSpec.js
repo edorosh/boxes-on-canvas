f1286f8f1179ff501882e1abfe73161fa9e30756
Eugene Dorosh <eugene.dorosh@gmail.com>
2019-02-14 01:11:20 +0300

    Fix all test cases. Add data provider

--- spec/shapeSpec.js
+++ spec/shapeSpec.js
@@ -1,153 +1,115 @@
+import using from 'jasmine-data-provider'
 import Shape from '../src/shape'
 
-describe('Class Shape(), represents a shape to be drawn on a Canvas', () => {
-  const stickyOffset = 10
-  let shape, collideShapeList, notCollideShapeList, stickToShapeList, notStickToShapeList
+describe('Class Shape', () => {
+  const shapeX = 5
+  const shapeY = 4
+  const shapeW = 2
+  const shapeH = 1
+  const halfWidth = parseInt(shapeW / 2, 10)
 
-  beforeEach(() => {
-    shape = new Shape(10, 20, 100, 200)
-    collideShapeList = [
-      new Shape(
-        shape.getOffsetX() - 10,
-        shape.getOffsetY(),
-        100,
-        200
-      ),
-      new Shape(
-        shape.getOffsetX(),
-        shape.getOffsetY() - 10,
-        100,
-        200
-      ),
-      new Shape(
-        shape.getOffsetX() - 10,
-        shape.getOffsetY() - 10,
-        100,
-        200
+  const snapToOffset = 1
+
+  const shape = new Shape(shapeX, shapeY, shapeW, shapeH)
+
+  describe('Should return coordinates', () => {
+    it('should return x coordinate', () => {
+      expect(shape.getX()).toBe(shapeX)
+    })
+
+    it('should return y coordinate', () => {
+      expect(shape.getY()).toBe(shapeY)
+    })
+
+    it('should return width', () => {
+      expect(shape.getWidth()).toBe(shapeW)
+    })
+
+    it('should return height', () => {
+      expect(shape.getHeight()).toBe(shapeH)
+    })
+
+    it('should return offset X', () => {
+      expect(shape.getOffsetX()).toBe(shapeX + shapeW)
+    })
+
+    it('should return offset Y', () => {
+      expect(shape.getOffsetY()).toBe(shapeY + shapeH)
+    })
+  })
+
+  describe('Should be drawable', () => {
+    it('by Canvas', () => {
+      let ctx = jasmine.createSpyObj('ctx', ['fillRect', 'fillStyle'])
+
+      shape.display(ctx)
+
+      expect(ctx.fillStyle).toBe(Shape.defaultFillColor)
+      expect(ctx.fillRect).toHaveBeenCalledWith(shapeX, shapeY, shapeW, shapeH)
+    })
+  })
+
+  describe('Should detect collisions', () => {
+    using({
+      'itself': new Shape(shape.getX(), shape.getY(), shapeW, shapeH),
+      'shift right': new Shape(shape.getX() + halfWidth, shape.getY(), shapeW, shapeH),
+      'shift left': new Shape(shape.getX() - halfWidth, shape.getY(), shapeW, shapeH),
+      'transform': new Shape(shape.getX(), shape.getY(), shapeH, shapeW),
+      'transform and shift right': new Shape(shape.getX() + halfWidth, shape.getY(), shapeH, shapeW),
+      'transform and shift up': new Shape(shape.getX(), shape.getY() - halfWidth, shapeH, shapeW),
+      'transform and shift up right': new Shape(shape.getX() + halfWidth, shape.getY() - halfWidth, shapeH, shapeW)
+    }, (overlapShape, description) => {
+      it(`${overlapShape} with the ${shape} (${description})`, () => {
+        expect(shape.collidesWith(overlapShape)).toBeTruthy()
+      })
+    })
+  })
+
+  describe('Should NOT detect collisions', () => {
+    using({
+      'move up': new Shape(shape.getX(), shape.getY() - halfWidth, shapeW, shapeH),
+      'move down': new Shape(shape.getX(), shape.getY() + halfWidth, shapeW, shapeH),
+      'move left': new Shape(shape.getX() - shape.getWidth(), shape.getY(), shapeW, shapeH),
+      'move right': new Shape(shape.getX() + shape.getWidth(), shape.getY(), shapeW, shapeH),
+      'transform and move left': new Shape(shape.getX() - halfWidth, shape.getY() - halfWidth, shapeH, shapeW),
+      'transform and move right': new Shape(shape.getX() + shape.getWidth(), shape.getY() - halfWidth, shapeH, shapeW)
+    }, (borderShape, description) => {
+      it(`${borderShape} with the ${shape} (${description})`, () => {
+        expect(shape.collidesWith(borderShape)).toBeFalsy()
+      })
+    })
+  })
+
+  describe('Should allow snapping', () => {
+    using({
+      'move up': new Shape(shape.getX(), shape.getY() - shape.getHeight() - snapToOffset, shapeW, shapeH),
+      'move down': new Shape(shape.getX(), shape.getOffsetY() + snapToOffset, shapeW, shapeH),
+      'move left': new Shape(shape.getX() - shape.getWidth() - snapToOffset, shape.getY(), shapeW, shapeH),
+      'move right': new Shape(shape.getX() + shape.getWidth() + snapToOffset, shape.getY(), shapeW, shapeH),
+      'transform and move up': new Shape(shape.getX(), shape.getY() - shape.getWidth() - snapToOffset, shapeH, shapeW),
+      'edge case': new Shape(
+        shape.getX() + shape.getWidth() + snapToOffset,
+        shape.getY() - shape.getHeight() - snapToOffset,
+        shapeW,
+        shapeH
       )
-    ]
-    notCollideShapeList = [
-      new Shape(
-        shape.getOffsetX(),
-        shape.getOffsetY(),
-        100,
-        200
-      ),
-      new Shape(
-        shape.getOffsetX() + 1,
-        shape.getOffsetY(),
-        100,
-        200
-      ),
-      new Shape(
-        shape.getOffsetX(),
-        shape.getOffsetY() + 1,
-        100,
-        200
-      ),
-      new Shape(
-        shape.getOffsetX() + 1,
-        shape.getOffsetY() + 1,
-        100,
-        200
-      )
-    ]
-    stickToShapeList = [
-      new Shape(
-        shape.getOffsetX() + stickyOffset,
-        shape.getOffsetY(),
-        100,
-        200
-      ),
-      new Shape(
-        shape.getOffsetX(),
-        shape.getOffsetY() + stickyOffset,
-        100,
-        200
-      ),
-      new Shape(
-        shape.getOffsetX() + stickyOffset,
-        shape.getOffsetY() + stickyOffset,
-        100,
-        200
-      )
-    ]
-    notStickToShapeList = [
-      new Shape(
-        shape.getOffsetX() + stickyOffset + 1,
-        shape.getOffsetY(),
-        100,
-        200
-      ),
-      new Shape(
-        shape.getOffsetX(),
-        shape.getOffsetY() + stickyOffset + 1,
-        100,
-        200
-      ),
-      new Shape(
-        shape.getOffsetX() + stickyOffset + 1,
-        shape.getOffsetY() + stickyOffset + 1,
-        100,
-        200
-      )
-    ]
+    }, (snapToShape, description) => {
+      it(`${snapToShape} to the ${shape} (${description})`, () => {
+        expect(snapToShape.isStickableTo(shape, snapToOffset)).toBeTruthy()
+      })
+    })
   })
 
-  it('should return x coordinate', () => {
-    expect(shape.getX()).toBe(10)
-  })
-
-  it('should return y coordinate', () => {
-    expect(shape.getY()).toBe(20)
-  })
-
-  it('should return width', () => {
-    expect(shape.getWidth()).toBe(100)
-  })
-
-  it('should return height', () => {
-    expect(shape.getHeight()).toBe(200)
-  })
-
-  it('should return offset X', () => {
-    expect(shape.getOffsetX()).toBe(10 + 100)
-  })
-
-  it('should return offset Y', () => {
-    expect(shape.getOffsetY()).toBe(20 + 200)
-  })
-
-  it('should display by context', () => {
-    let ctx = jasmine.createSpyObj('ctx', ['fillRect', 'fillStyle'])
-
-    shape.display(ctx)
-
-    expect(ctx.fillStyle).toBe(Shape.defaultFillColor)
-    expect(ctx.fillRect).toHaveBeenCalledWith(10, 20, 100, 200)
-  })
-
-  it('should detect collisions with shapes', () => {
-    for (let shapeInCollision of collideShapeList) {
-      expect(shape.collidesWith(shapeInCollision)).toBeTruthy()
-    }
-  })
-
-  it('should not detect collisions with shapes', () => {
-    for (let shapeNotInCollision of notCollideShapeList) {
-      expect(shape.collidesWith(shapeNotInCollision)).toBeFalsy()
-    }
-  })
-
-  it('should allow sticking itself to the shape', () => {
-    for (let shapeToBeSticked of stickToShapeList) {
-      expect(shapeToBeSticked.isStickableTo(shape, stickyOffset)).toBeTruthy()
-    }
-  })
-
-  it('should not allow sticking itself to the shape', () => {
-    for (let shapeToBeSticked of stickToShapeList) {
-      expect(shapeToBeSticked.isStickableTo(shape, stickyOffset)).toBeTruthy()
-    }
+  describe('Should NOT allow snapping', () => {
+    using({
+      'move up': new Shape(shape.getX(), shape.getY() - shape.getHeight() - snapToOffset - 1, shapeW, shapeH),
+      'move down': new Shape(shape.getX(), shape.getOffsetY() + snapToOffset + 1, shapeW, shapeH),
+      'move left': new Shape(shape.getX() - shape.getWidth() - snapToOffset - 1, shape.getY(), shapeW, shapeH),
+      'move right': new Shape(shape.getOffsetX() + snapToOffset + 1, shape.getY(), shapeW, shapeH)
+    }, (notSnapToShape, description) => {
+      it(`${notSnapToShape} to the ${shape} (${description})`, () => {
+        expect(notSnapToShape.isStickableTo(shape, snapToOffset)).toBeFalsy()
+      })
+    })
   })
 })
--- src/shape.js
+++ src/shape.js
@@ -73,68 +73,32 @@ export default class Shape {
    * @returns {boolean}
    */
   static collides (a, b) {
-    return (a.getX() < b.getOffsetX() && a.getOffsetX() > b.getX()) ||
+    return (a.getX() < b.getOffsetX() && a.getOffsetX() > b.getX()) &&
       (a.getY() < b.getOffsetY() && a.getOffsetY() > b.getY())
   }
 
   /**
    * Detects if two Shape can be glued together
    *
-   * @param {Shape} shapeToBeSticked
-   * @param {Shape} shape
+   * @param {Shape} a
+   * @param {Shape} b
    * @param {int} stickyOffset
    *
    * @returns {boolean}
    */
-  static areStickable (shapeToBeSticked, shape, stickyOffset) {
-    stickyOffset++
+  static areStickable (a, b, stickyOffset) {
+    const boostedShape = new Shape(
+      b.getX() - stickyOffset,
+      b.getY() - stickyOffset,
+      b.getWidth() + stickyOffset * 2,
+      b.getHeight() + stickyOffset * 2
+    )
 
-    /**
-     * @type {Shape[]}
-     */
-    let shiftedShapeList = [
-      new Shape(
-        shape.getX(),
-        shape.getY() + stickyOffset,
-        shape.getWidth(),
-        shape.getHeight()
-      ),
-      new Shape(
-        shape.getX() + stickyOffset,
-        shape.getY(),
-        shape.getWidth(),
-        shape.getHeight()
-      )
-    ]
+    return (boostedShape.getX() <= a.getOffsetX() && boostedShape.getOffsetX() >= a.getX()) &&
+      (boostedShape.getY() <= a.getOffsetY() && boostedShape.getOffsetY() >= a.getY())
+  }
 
-    if (shape.getX() - stickyOffset > 0) {
-      shiftedShapeList.push(
-        new Shape(
-          shape.getX() - stickyOffset,
-          shape.getY(),
-          shape.getWidth(),
-          shape.getHeight()
-        )
-      )
-    }
-
-    if (shape.getY() - stickyOffset > 0) {
-      shiftedShapeList.push(
-        new Shape(
-          shape.getX(),
-          shape.getY() - stickyOffset,
-          shape.getWidth(),
-          shape.getHeight()
-        )
-      )
-    }
-
-    for (let shiftedShape of shiftedShapeList) {
-      if (shiftedShape.collidesWith(shapeToBeSticked)) {
-        return true
-      }
-    }
-
-    return false
+  toString () {
+    return `Shape(${this.x}, ${this.y}, ${this.width}, ${this.heigh}, ${this.fill})`
   }
 }
